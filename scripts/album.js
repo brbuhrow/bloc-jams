e46ff5b7188649c7c22bdfd98c0f7f451379c347
Ben Buhrow <brbuhrow@gmail.com>
2017-07-12 10:39:49 -0600

    Actual Checkpoint 20 Complete

--- scripts/album.js
+++ scripts/album.js
@@ -1,6 +1,24 @@
 var setSong = function(songNumber) {
+  if (currentSoundFile) {
+           currentSoundFile.stop();
+       }
+
   currentlyPlayingSongNumber = parseInt(songNumber);
   currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+
+  currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+
+         formats: [ 'mp3' ],
+         preload: true
+     });
+
+     setVolume(currentVolume);
+};
+
+var setVolume = function(volume) {
+    if (currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+    }
 };
 
 var getSongNumberCell = function(number) {
@@ -28,13 +46,20 @@ var getSongNumberCell = function(number) {
       }
 
      if (currentlyPlayingSongNumber !== songNumber) {
+          currentSoundFile.play();
           $(this).html(pauseButtonTemplate);
           setSong(songNumber);
           updatePlayerBarSong();
      } else if (currentlyPlayingSongNumber === songNumber) {
-          $(this).html(playButtonTemplate);
-          $('.main-controls .play-pause').html(playerBarPlayButton);
-          setSong(songNumber);
+       if (currentSoundFile.isPaused()) {
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
+                currentSoundFile.play();
+            } else {
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();
+            }
       }
 
   };
@@ -107,6 +132,7 @@ var trackIndex = function(album, song) {
  var currentlyPlayingSongNumber = null;
  var currentSongFromAlbum = null;
  var currentSoundFile = null;
+ var currentVolume = 80;
 
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
@@ -137,7 +163,8 @@ var trackIndex = function(album, song) {
 
         var lastSongNumber = currentlyPlayingSongNumber;
 
-        currentlyPlayingSongNumber = currentSongIndex + 1;
+        setSong(currentSongIndex + 1);
+        currentSoundFile.play();
         currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
         updatePlayerBarSong();
@@ -159,7 +186,8 @@ var trackIndex = function(album, song) {
 
         var lastSongNumber = currentlyPlayingSongNumber;
 
-        currentlyPlayingSongNumber = currentSongIndex + 1;
+        setSong(currentSongIndex + 1);
+        currentSoundFile.play();
         currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
         updatePlayerBarSong();
